45692c1831150e66656c00af812e70ce0b506b72
Diego Nascimento <diegostealth3@gmail.com>
2022-11-11 22:31:26 -0300

    ajustes textos da sessao 1

--- src/modules/Home/Components/Section1/components/SubTitle/SubTitle.tsx
+++ src/modules/Home/Components/Section1/components/SubTitle/SubTitle.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const SubTitle = () => {
   return (
-    <h2 className="text-slate-400 text-2xl md:text-4xl font-semibold mt-5 text-center w-full">
+    <h2 className="text-slate-400 text-2xl md:text-2xl font-semibold mt-5 text-center w-full max-w-[800px]">
       Divi Takes WordPress To A Whole New Level With Its Incredibly Advanced
       Visual Builder Technology
     </h2>
--- src/modules/Home/Components/Section1/components/Title/Title.tsx
+++ src/modules/Home/Components/Section1/components/Title/Title.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Title = () => {
   return (
-    <h1 className="text-white text-6xl md:text-9xl font-semibold text-center">
+    <h1 className="text-white text-6xl md:text-7xl font-semibold text-center">
       Build Visualy
     </h1>
   );
