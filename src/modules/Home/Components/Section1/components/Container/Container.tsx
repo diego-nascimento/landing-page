9f10e7c37656b644197018b5c39db66ffa7add16
Diego Nascimento <diegostealth3@gmail.com>
2022-11-11 22:36:23 -0300

    incluindo fullscreencontainer paralax

--- src/modules/Home/Components/Section1/components/Container/Container.tsx
+++ src/modules/Home/Components/Section1/components/Container/Container.tsx
@@ -19,6 +19,7 @@ export const Container = ({ children }: Props) => {
             url: 'http://localhost:3000/fundo-teste.jpg',
           },
         }}
+        paralax={true}
       >
         <div className="flex flex-col justify-center items-center w-full h-full px-5 mt-10 md:mt-20 ">
           <NotFullWidthContainer centered>{children}</NotFullWidthContainer>
--- src/shared/components/Container/Components/fullScreenContainer/FullScreenContainer.tsx
+++ src/shared/components/Container/Components/fullScreenContainer/FullScreenContainer.tsx
@@ -1,18 +1,21 @@
 import { useIsMobile } from '@/shared/hooks/isMobile/useIsMobile';
-import React from 'react';
+import React, { useMemo } from 'react';
 import { imageBackgroundContainerProps } from '../types';
 
 interface Props {
   children?: React.ReactNode;
   backgroundImages: imageBackgroundContainerProps;
   backgroundColor: string;
+  paralax?: boolean;
 }
 
-export const FullScreenContainer = (data: Props) => {
+export const FullScreenContainer = ({ paralax = false, ...data }: Props) => {
   const { isMobile } = useIsMobile();
+
+  const paralaxEffect = useMemo(() => (paralax ? 'bg-fixed' : ''), [paralax]);
   return (
     <section
-      className={`bg-[${data.backgroundColor}]  w-screen h-full min-h-screen bg-no-repeat bg-center bg-cover`}
+      className={`bg-[${data.backgroundColor}]  w-screen h-full min-h-screen bg-no-repeat bg-center bg-cover ${paralaxEffect}`}
       style={{
         backgroundImage: `url(${
           isMobile
